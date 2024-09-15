1ce889fbbec1caef0f83803bacdaa8fc39fcf993
Pyry Lehtinen <lehtinen.pyry@outlook.com>
2024-09-15 15:28:34 +0300

    Moved Device Data grid to its own file, and Events History pageto use it.

--- frontend/react_app/src/pages/device_history_page.js
+++ frontend/react_app/src/pages/device_history_page.js
@@ -1,16 +1,13 @@
 import * as React from 'react';
-import useState from 'react';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
-import TextField from '@mui/material/TextField'
-import { DataGrid} from '@mui/x-data-grid';
-import TopMenu from '../components/top_menu'
-import DeviceHistoryCard from '../components/card'
-import DeviceHistoryContainer from '../components/page_container'
+import TopMenu from '../components/top_menu';
+import DeviceHistoryCard from '../components/card';
+import DeviceHistoryContainer from '../components/page_container';
 import { useFetchData } from '../services/fetch_data';
+import SearchableDataGrid from '../components/searchable_data_grid';
 
 function DeviceHistoryPage() {
-  const [searchDevices, setSearchDevices] = useState('');
   const { data: devices, loading: devicesLoading, error: devicesError }
     = useFetchData('http://localhost:5000/devices');
   const { data: events, loading: eventsLoading, error: eventsError } 
@@ -18,41 +15,9 @@ function DeviceHistoryPage() {
   const { data: users, loading: usersLoading, error: usersError } 
     = useFetchData('http://localhost:5000/users');
 
-  const columns = [
-    { field: 'id', headerName: 'id', width: 50, headerAlign: 'center' },
-    { field: 'type', headerName: 'type', width: 50, headerAlign: 'center' },
-    { field: 'deviceName', headerName: 'Device name'
-                                          , width: 125, headerAlign: 'center' },
-    { field: 'location', headerName: 'LOC', width: 125, headerAlign: 'center' },
-    { field: 'opt1', headerName: 'Optional 1', width: 100 },
-    { field: 'opt2', headerName: 'Optional 2', width: 100 },
-    { field: 'opt3', headerName: 'Optional 3', width: 100 },
-  ];
-
-
-  const filterDevices = (!devicesLoading && devices) ?
-   devices.filter((device) =>
-    device.dev_name.toLowerCase().includes(searchDevices.toLowerCase()) ||
-    device.dev_type.toLowerCase().includes(searchDevices.toLowerCase()) ||
-    device.dev_id.toLowerCase().includes(searchDevices.toLowerCase())
-    // Filter by location
-  ) : [];
-  
-  const rows = filterDevices.map((device) => ({
-    id: device.dev_id,
-    type: device.dev_type,
-    deviceName: device.dev_name,
-    location: 'WIP',
-    opt1: device.optional_info[0]?.opt1 || 'N/A',
-    opt2: device.optional_info[0]?.opt2 || 'N/A',
-    opt3: device.optional_info[0]?.opt3 || 'N/A',
-  }));
-
-
   // Value will eventually be imported from the QR Code
   const deviceNumber = (devices && devices.length > 0) ?
     Math.floor(Math.random() * devices.length) : 0;
-
 
   return (
     <DeviceHistoryContainer direction="column" justifyContent="space-between">
@@ -106,36 +71,11 @@ function DeviceHistoryPage() {
             </Typography>
           )}
         </Box>
-        <Box
-          sx={{
-            display: 'flex',
-            flexDirection: 'column',
-            width: '100%',
-            gap: 2,
-          }}>
-          {(!devicesLoading) ? (
-            <>
-          <TextField
-            label="Search Devices"
-            variant="outlined"
-            value={searchDevices}
-            onChange={(e) => setSearchDevices(e.target.value)} 
-            sx={{ marginBottom: 2 }}
-          />
-            <DataGrid
-              rows={rows}
-              columns={columns}
-              autoHeight
-              pageSize={5}
-              rowsPerPageOptions={[5]}
-            />
-          </>
-          ) : (
-            <Typography component="h1" variant="h4">
-              Loading data...
-            </Typography>
-          )}
-        </Box>
+        <SearchableDataGrid 
+        devices={devices} 
+        devicesLoading={devicesLoading} 
+        devicesError={devicesError} 
+      />
       </DeviceHistoryCard>
     </DeviceHistoryContainer>
   );
