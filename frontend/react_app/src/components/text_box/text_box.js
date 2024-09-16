a1d0dbfeba9b5b17ad11cb56b27714c6f4d33c3c
Pyry Lehtinen <lehtinen.pyry@outlook.com>
2024-09-16 22:45:28 +0300

    Using /components text_box and form_input on the pages.

--- frontend/react_app/src/components/text_box/text_box.js
+++ frontend/react_app/src/components/text_box/text_box.js
@@ -1,24 +1,32 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
 
-import MuiCard from '@mui/material/Card';
-import { styled } from '@mui/material/styles';
+const InfoBox = ({texts}) => {
+  return (
+    <Box sx={{ 
+      display: 'flex', 
+      flexDirection: 'column', 
+      justifyContent: 'center', 
+      alignItems: 'center', 
+      mt: 2, 
+      maxWidth: '100%'
+    }}>
+      {texts.map((text, index) => (
+        <Typography
+          key={index}
+          sx={{ 
+            fontSize: 'clamp(1rem, 5vw, 1.9rem)', 
+            paddingY: 1,
+            textAlign: 'center',
+            overflow: 'hidden',
+          }}
+        >
+          {text}
+        </Typography>
+      ))}
+    </Box>
+  )
+};
 
-const PageCard = styled(MuiCard)(({ theme }) => ({
-  display: 'flex',
-  flexDirection: 'column',
-  alignSelf: 'center',
-  width: '100%',
-  padding: theme.spacing(4),
-  gap: theme.spacing(2),
-  margin: 'auto',
-  [theme.breakpoints.up('sm')]: {
-    maxWidth: '450px',
-  },
-  boxShadow:
-    'hsla(220, 30%, 5%, 0.05) 0px 5px 15px 0px, hsla(220, 25%, 10%, 0.05) 0px 15px 35px -5px',
-  ...theme.applyStyles('dark', {
-    boxShadow:
-      'hsla(220, 30%, 5%, 0.5) 0px 5px 15px 0px, hsla(220, 25%, 10%, 0.08) 0px 15px 35px -5px',
-  }),
-}));
-
-export default PageCard;
+export default InfoBox;
