966558c8746ba821ce00c0a72c205790d6533bc6
Pyry Lehtinen <lehtinen.pyry@outlook.com>
2024-09-17 14:18:36 +0300

    Slightly modified fetch_data and created tests for it.

--- frontend/react_app/src/services/fetch_data/fetch_data.js
+++ frontend/react_app/src/services/fetch_data/fetch_data.js
@@ -1,9 +1,11 @@
 import { useState, useEffect } from 'react';
 
-export const useFetchData = (url) => {
+export const useFetchData = (endpoint) => {
   const [data, setData] = useState([]);
   const [loading, setLoading] = useState(true); 
   const [error, setError] = useState(null); 
+
+  const url = `http://localhost:5000/${endpoint}`
 
   useEffect(() => {
     const fetchData = async () => {
@@ -12,14 +14,14 @@ export const useFetchData = (url) => {
         const result = await response.json();
         setData(result); 
       } catch (err) {
-        setError(err.message);
+        setError(err);
       } finally {
         setLoading(false); // Used by pages to verify data is readable.
       }
     };
     fetchData();
 
-  }, [url]);
+  }, [endpoint]);
 
   return { data, loading, error }; // Error not used currently but good to have.
 };
