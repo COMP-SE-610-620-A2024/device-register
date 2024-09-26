755159bba8b70d4b0c7a41ecb291ca2cb348eef8
Pyry Lehtinen <lehtinen.pyry@outlook.com>
2024-09-27 01:18:15 +0300

    Learning how to do a PR after coding for 2 weeks without one.

--- frontend/src/views/admin_view.jsx
+++ frontend/src/views/admin_view.jsx
@@ -1,15 +1,42 @@
 import Box from '@mui/material/Box';
-import { useFetchData } from '../components/shared/fetch_data';
+import { useFetchData } from '../services/fetch_data/fetch_data';
 
-
-function AdminView() {
-
-
+function Admin_view() {
   return (
     <div>
-
+      <Box sx={{ 
+        display: 'flex',
+        flexDirection: 'row',
+        alignItems: 'center',
+        gap: 2 
+      }}>
+        <a
+          className="App-link"
+          href="/"
+          target="_self"
+          rel="noopener noreferrer"
+        >
+          Login
+        </a>
+        <a
+          className="App-link"
+          href="/home"
+          target="_self"
+          rel="noopener noreferrer"
+        >
+          Home
+        </a>
+        <a
+          className="App-link"
+          href="/device_history"
+          target="_self"
+          rel="noopener noreferrer"
+        >
+          Device History
+        </a>
+      </Box>
     </div>
   )
 }
   
-export default AdminView;
+export default Admin_view;
