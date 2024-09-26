cea8d8144681cf4957e8b05a1226c5c7ce42f8e4
Ville Herajärvi <ville.herajarvi@outlook.com>
2024-09-26 14:12:09 +0300

    Eslint config

---
+++ frontend/eslint.config.js
@@ -0,0 +1,13 @@
+// eslint.config.js
+export default {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: 'eslint:recommended',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {},
+};
