47192313b104fb63f1349da87c9cf678c698fb43
Pyry Lehtinen <lehtinen.pyry@outlook.com>
2024-09-27 20:15:18 +0300

    Functionality and unit test for link_button for PR.

--- frontend/src/components/shared/link_button.jsx
+++ frontend/src/components/shared/link_button.jsx
@@ -1,14 +1,38 @@
-import { useNavigate } from 'react-router-dom';
-import Button from '@mui/material/Button';
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Button } from '@mui/material';
 
-const Link_button = ({}) => {
-  
-
+{/* Extending MUI Button to work as a link button.*/}
+const LinkButton = ({ href, text, icon, target = "_self", iconSx,
+   variant = "contained", size = "medium", color = "primary" }) => {
   return (
-    <Button>
-        
+    <Button
+      component="a"
+      href={href}
+      target={target}
+      rel="noopener noreferrer"
+      startIcon={icon}
+      iconSx={iconSx}
+      variant={variant}
+      size={size}
+      color={color}
+      role="button" // Ensure tests treat it as button.
+    >
+      {text}
     </Button>
   );
 };
 
-export default Link_button;
+LinkButton.propTypes = {
+  href: PropTypes.string.isRequired,
+  text: PropTypes.string,
+  icon: PropTypes.node,
+  target: PropTypes.string,
+  iconSx: PropTypes.object,
+  variant: PropTypes.oneOf(["text", "outlined", "contained"]),
+  size: PropTypes.oneOf(["small", "medium", "large"]),
+  color: PropTypes.oneOf(
+    ["primary", "secondary", "error", "info", "success", "warning"]),
+};
+
+export default LinkButton;
