32265ae1260cbf1dbf0a9df9844f4b0351e41f5e
Samantha Bavautdin <samantha.bavautdin@tuni.fi>
2024-09-29 00:28:52 +0300

    Event table implemented

---
+++ backend/models/event_model.py
@@ -0,0 +1,19 @@
+from backend.utils.database_Init import db
+
+class Event(db.Model):
+    __tablename__: str = 'events'
+
+    event_id = db.Column(db.Integer, primary_key=True)
+    dev_id = db.Column(db.Integer, db.ForeignKey('devices.dev_id'), nullable=False)
+    user_id = db.Column(db.Integer, nullable=False) # TODO: Add db.ForeignKey('users.user_id')
+    move_time = db.Column(db.DateTime, nullable=False)
+    loc_name = db.Column(db.String(200), nullable=False)
+
+    def to_dict(self) -> dict[str, str]:
+        return {
+            'event_id': str(self.event_id),
+            'dev_id': str(self.dev_id),
+            'user_id': str(self.user_id),
+            'move_time': str(self.move_time),
+            'loc_name': str(self.loc_name)
+        }
