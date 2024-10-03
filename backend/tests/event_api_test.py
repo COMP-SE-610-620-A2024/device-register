8f77e3541a1f9336c8a5bc5929be076d79481088
Ville Herajärvi <ville.herajarvi@outlook.com>
2024-10-03 14:30:32 +0300

    Move setup to own folder

--- backend/tests/event_api_test.py
+++ backend/tests/event_api_test.py
@@ -1,6 +1,6 @@
 import pytest
 from backend.app import create_app
-from backend.utils.database_Init import db
+from backend.setup.database_Init import db
 from backend.models.device_model import Device
 from backend.models.event_model import Event
 from sqlalchemy.sql import func
