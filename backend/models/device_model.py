72eba81bce2f540a6843e9558478b9daf806ec96
Ville Herajärvi <ville.herajarvi@outlook.com>
2024-10-21 17:23:22 +0300

    Fix lint

--- backend/models/device_model.py
+++ backend/models/device_model.py
@@ -1,6 +1,4 @@
-from typing import Union, Tuple, Any
-
-from sqlalchemy.orm import RelationshipProperty
+from typing import Union
 
 from backend.setup.database_Init import db
 from backend.models.event_model import Event
@@ -87,7 +85,8 @@ class Device(db.Model):
             return 500, str(error)
 
     @staticmethod
-    def get_events_by_device_id(dev_id: int) -> Union[tuple[list[Event], int], tuple[None, int]]:
+    def get_events_by_device_id(dev_id: int)\
+            -> Union[tuple[list[Event], int], tuple[None, int]]:
         device = Device.get_device_by_id(dev_id)
         if device:
             return device.events, 200
