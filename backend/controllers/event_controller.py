9b8388e2902792870247973a5915e216f437fc43
Samantha Bavautdin <samantha.bavautdin@tuni.fi>
2024-10-22 18:34:53 +0300

    Create event accepts lists implemented and tested

--- backend/controllers/event_controller.py
+++ backend/controllers/event_controller.py
@@ -33,53 +33,66 @@ def get_event_by_id(event_id: int) -> tuple[Response, int]:
 
 
 def create_event() -> tuple[Response, int]:
-    event_json = request.get_json()
+    request_json = request.get_json()
 
-    if not isinstance(event_json, dict):
-        return jsonify({'error': "Expected an event object"}), 400
+    if not isinstance(request_json, (dict, list)):
+        return jsonify({'error': "Expected an event object or list"}), 400
 
-    if not all(key in event_json for key in ('dev_id',
-                                             'user',
-                                             'move_time',
-                                             'loc_name',
-                                             'comment')):
-        return (jsonify({'error': "Event must have"
-                                  " dev_id,"
-                                  " move_time,"
-                                  " loc_name,"
-                                  " comment, and"
-                                  " user"}),
-                400)
-
-    try:
-        move_time = datetime.strptime(event_json['move_time'],
-                                      '%Y-%m-%d %H:%M:%S')
-    except ValueError:
-        return jsonify({'error': "Expected time format: YYYY-MM-DD HH:MM:SS"}), 400
-
-    user_info = event_json['user']
-
-    if not isinstance(user_info, dict):
-        return jsonify({'error': "Expected user attribute to be object"}), 400
-
-    if not all(key in user_info for key in ('user_name', 'user_email')):
-        return jsonify({'error': "User object must contain name and email"}), 400
-
-    not_ok_status, response_str = add_or_update_user_util(user_info)
-    if not_ok_status:
-        return jsonify({'error': f'Problem adding or updating user: '
-                                 f'{response_str}'
-                        }), not_ok_status
+    # turn dict into a list, or check list contains dicts
+    if isinstance(request_json, dict):
+        event_json_list = [request_json]
     else:
-        user_id = response_str
+        event_json_list = request_json
+        for item in event_json_list:
+            if not isinstance(item, dict):
+                return jsonify({'error': "Expected event objects in the list"}), 400
 
-    new_event = Event(dev_id=event_json['dev_id'],
-                      user_id=user_id,
-                      move_time=move_time,
-                      loc_name=event_json['loc_name'],
-                      comment=event_json['comment'])
+    event_list = []
 
-    database_response = Event.create_event(new_event)
+    for event_json in event_json_list:
+        if not all(key in event_json for key in ('dev_id',
+                                                 'user',
+                                                 'move_time',
+                                                 'loc_name',
+                                                 'comment')):
+            return (jsonify({'error': "Event must have"
+                                      " dev_id,"
+                                      " move_time,"
+                                      " loc_name,"
+                                      " comment, and"
+                                      " user"}),
+                    400)
+
+        try:
+            move_time = datetime.strptime(event_json['move_time'],
+                                          '%Y-%m-%d %H:%M:%S')
+        except ValueError:
+            return jsonify({'error': "Expected time format: YYYY-MM-DD HH:MM:SS"}), 400
+
+        user_info = event_json['user']
+
+        if not isinstance(user_info, dict):
+            return jsonify({'error': "Expected user attribute to be object"}), 400
+
+        if not all(key in user_info for key in ('user_name', 'user_email')):
+            return jsonify({'error': "User object must contain name and email"}), 400
+
+        not_ok_status, response_str = add_or_update_user_util(user_info)
+        if not_ok_status:
+            return jsonify({'error': f'Problem adding or updating user: '
+                                     f'{response_str}'
+                            }), not_ok_status
+        else:
+            user_id = response_str
+
+        new_event = Event(dev_id=event_json['dev_id'],
+                          user_id=user_id,
+                          move_time=move_time,
+                          loc_name=event_json['loc_name'],
+                          comment=event_json['comment'])
+        event_list.append(new_event)
+
+    database_response = Event.create_event(event_list)
     if database_response[0]:
         return jsonify({'message': "Event created successfully"}), 201
     else:
