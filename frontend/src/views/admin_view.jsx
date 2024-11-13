ee368995780867cdea061ef886303295b1d6dd2f
Aleksi Kilpilampi <aleksi.kilpilampi@tuni.fi>
2024-11-13 22:53:00 +0200

    admin_view and tests

--- frontend/src/views/admin_view.jsx
+++ frontend/src/views/admin_view.jsx
@@ -35,34 +35,17 @@ function Admin_view() {
     <div>
       <Box sx={{ 
         display: 'flex',
-        flexDirection: 'row',
+        flexDirection: 'column',
         alignItems: 'center',
-        gap: 2 
+        gap: 4 
       }}>
-        <a
-          className="App-link"
-          href="/"
-          target="_self"
-          rel="noopener noreferrer"
-        >
-          Login
-        </a>
-        <a
-          className="App-link"
-          href="/home"
-          target="_self"
-          rel="noopener noreferrer"
-        >
-          Home
-        </a>
-        <a
-          className="App-link"
-          href="/device_history"
-          target="_self"
-          rel="noopener noreferrer"
-        >
-          Device History
-        </a>
+        <NavigationBar/>
+        <Typography sx={{ fontSize: 'clamp(1.2rem, 3vw, 1.8rem)', mt: 8 }}>
+          Management pages
+        </Typography>
+        <Link_button href={`/`} text= "Devices"></Link_button>
+        <Link_button href={`/events`} text= "Events"></Link_button>
+        <Link_button href={`/`} text= "Backups"></Link_button>
       </Box>
     </div>
   )
