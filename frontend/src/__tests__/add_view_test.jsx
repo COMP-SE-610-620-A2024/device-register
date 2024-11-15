26221b72ba1062920323ce84aaa3dabf6cb0771d
Aleksi Kilpilampi <aleksi.kilpilampi@tuni.fi>
2024-11-15 23:38:56 +0200

    test eslint error fixed???

--- frontend/src/__tests__/add_view_test.jsx
+++ frontend/src/__tests__/add_view_test.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, screen, fireEvent } from '@testing-library/react';
+import { render, screen} from '@testing-library/react';
 import Add_View from "../views/add_view";
 import '@testing-library/jest-dom';
 import { MemoryRouter } from 'react-router-dom';
