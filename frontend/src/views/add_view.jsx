198c34fa791d8de0a7a0a5671a20638f1d06437f
Aleksi Kilpilampi <aleksi.kilpilampi@tuni.fi>
2024-11-15 23:36:49 +0200

    fix eslint

--- frontend/src/views/add_view.jsx
+++ frontend/src/views/add_view.jsx
@@ -4,13 +4,13 @@ import { Typography, TextField, MenuItem, Select, FormControl, InputLabel  } fro
 import NavigationBar from '../components/shared/navigation_bar';
 import Form_container from '../components/shared/form_container';
 import Function_button from '../components/shared/function_button';
-import { useParams, useNavigate} from 'react-router-dom';
+import {useNavigate} from 'react-router-dom';
 import { useState, useEffect } from 'react';
 import useFetchData from '../components/shared/fetch_data';
 import usePostData from '../components/shared/post_data';
 const Add_view = () => {
   const navigate = useNavigate(); 
-  const { data: deviceClasses, loading, error } = useFetchData('classes/');
+  const { data: deviceClasses, loading} = useFetchData('classes/');
   const [errorMessage, setErrorMessage] = useState(null);
   
   
@@ -39,12 +39,6 @@ const Add_view = () => {
     })
   };
 
-  const handleClassChange = (e) => {
-    setDeviceData({
-      ...deviceData,
-      class_id: e.target.value,
-    });
-  };
 
   const onSubmit = (e) => {
     e.preventDefault()
