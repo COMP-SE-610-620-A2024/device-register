64d1c65901867f4dca653cd81dff2a6af031f4b8
Pyry Lehtinen <lehtinen.pyry@outlook.com>
2024-11-24 19:14:01 +0200

    Forgot one import

--- frontend/src/components/device_manager/confirmation_popup.jsx
+++ frontend/src/components/device_manager/confirmation_popup.jsx
@@ -5,7 +5,7 @@ import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
 import DialogContentText from '@mui/material/DialogContentText';
 import DialogTitle from '@mui/material/DialogTitle';
-import Function_button from '../shared/Function_button';
+import Function_button from '../shared/function_button';
 
 const ConfirmationPopup = forwardRef(({ renderTrigger, onConfirm, dialogTitle, dialogText }, ref) => {
   const [open, setOpen] = useState(false);
